e05c7967473b50bff16f77bb1144183ed8681d70
James Bithell <Jbithell@users.noreply.github.com>
2021-11-27 12:26:13 +0000

    Add tailwind & logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,16 +20,17 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bithell Family Website`,
+        short_name: `Bithell.com`,
         start_url: `/`,
         background_color: `#663399`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logo-square.png`, // This path is relative to the root of the site.
       },
-    }
+    },
+    `gatsby-plugin-postcss`,
   ],
 }
