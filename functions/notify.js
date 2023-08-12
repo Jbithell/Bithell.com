7b39854c09c59596134eaa89b3fbf85c6aa1e79d
James Bithell <Jbithell@users.noreply.github.com>
2023-08-12 09:19:45 +0100

    Add notify function

---
+++ functions/notify.js
@@ -0,0 +1,10 @@
+export default {
+  async fetch(request, env, ctx) {
+    let email = request.body;
+    let region = request.cf.region;
+    let value = await env.NOTIFY.get("test");
+    await env.NOTIFY.put(region + ":" + Date. now(), email);
+    // Return the value, as is, for the Response
+    return new Response(value + email + region);
+  },
+};
