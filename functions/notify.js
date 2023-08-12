a51274abbd90b4e593c3966c68322d0542f8d3ae
James Bithell <Jbithell@users.noreply.github.com>
2023-08-12 09:44:47 +0100

    Remove notify

--- functions/notify.js
+++
@@ -1,9 +0,0 @@
-export function onRequest(context) {
-  let email = request.body;
-  let region = request.cf.region;
-  let value = await env.NOTIFY.get("test");
-  await env.NOTIFY.put(region + ":" + Date. now(), email);
-  // Return the value, as is, for the Response
-  return new Response(value + email + region);
-
-};
