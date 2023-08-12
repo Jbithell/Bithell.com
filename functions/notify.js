dcc7ae1954e4d60486b5b3e522605e6c349e4af4
James Bithell <Jbithell@users.noreply.github.com>
2023-08-12 09:28:48 +0100

    Update notify.js

--- functions/notify.js
+++ functions/notify.js
@@ -1,10 +1,9 @@
-export default {
-  async fetch(request, env) {
-    let email = request.body;
-    let region = request.cf.region;
-    let value = await env.NOTIFY.get("test");
-    await env.NOTIFY.put(region + ":" + Date. now(), email);
-    // Return the value, as is, for the Response
-    return new Response(value + email + region);
-  },
+export function onRequest(context) {
+  let email = request.body;
+  let region = request.cf.region;
+  let value = await env.NOTIFY.get("test");
+  await env.NOTIFY.put(region + ":" + Date. now(), email);
+  // Return the value, as is, for the Response
+  return new Response(value + email + region);
+
 };
